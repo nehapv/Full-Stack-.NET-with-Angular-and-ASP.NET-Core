e95ecb27b95c571bdc7c9c733b55cd1933a2d554
Neha <nehaprakash398@gmail.com>
2026-08-31 01:03:09 +0100

    Add project files.

---
+++ Services/IProductService.cs
@@ -0,0 +1,14 @@
+﻿
+using MyFirstWebAPIProject.DTOs;
+        
+namespace MyFirstWebAPIProject.Services
+{
+    public interface IProductService
+    {
+        IEnumerable<ProductDTO> GetAllProducts();
+        ProductDTO? GetProductById(int id);
+        ProductDTO CreateProduct(ProductCreateDTO createDto);
+        bool UpdateProduct(int id, ProductUpdateDTO updateDto);
+        bool DeleteProduct(int id);
+    }
+}
