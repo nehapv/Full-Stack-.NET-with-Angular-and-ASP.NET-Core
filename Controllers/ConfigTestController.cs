042ded0cc620a25d297f12eed18cbbbb72f2a871
Neha <nehaprakash398@gmail.com>
2026-08-31 15:26:24 +0100

    Creating a Test Controller to Access Configuration

---
+++ Controllers/ConfigTestController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyFirstWebAPIProject.Models;
+
+namespace MyFirstWebAPIProject.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ConfigTestController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public ConfigTestController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        [HttpGet]
+        public IActionResult GetConfigValues()
+        {
+            // Method 1: Direct access using keys
+            var appName = _configuration["MyAppSettings:ApplicationName"];
+
+            // Method 2: Access using GetSection
+            var pageSize = _configuration.GetSection("MyAppSettings:DefaultPageSize").Value;
+
+            // Method 3: Binding to a strongly-typed object
+            // Here, the Key Names must be same as the Property Names
+            var settings = new MyAppSettings();
+            _configuration.GetSection("MyAppSettings").Bind(settings);
+
+            return Ok(new
+            {
+                ApplicationName = appName,
+                DefaultPageSize = pageSize,
+                StronglyTyped = settings
+            });
+        }
+    }
+}
+
---
+++ Models/MyAppSettings.cs
@@ -0,0 +1,9 @@
+﻿namespace MyFirstWebAPIProject.Models
+{
+    public class MyAppSettings
+    {
+        public string? ApplicationName { get; set; }
+        public string? Version { get; set; }
+        public int DefaultPageSize { get; set; }
+    }
+}
